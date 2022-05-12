d4e43390e3d71aa6b16c14c2af29fc6e8bfcb2ba
sugiymki <sugiyama@gfd-dennou.org>
2022-05-12 23:21:40 +0900

    update the class

--- mrblib/models/rc8035sa.rb
+++ mrblib/models/rc8035sa.rb
@@ -4,6 +4,7 @@
 # I2C address : 0x32
 
 class RX8035SA
+
   def initialize(i2c)
     @i2c = i2c
     @i2c.write(0x32, [0xE0, 0x00, 0x00])
@@ -11,12 +12,28 @@ class RX8035SA
   end
 
   def read
-    a = @i2c.read_integer(0x32, 8)   # 8 バイト分読み込み
+    a = @i2c.readfrom(0x32, 8)   # 8 バイト分読み込み
     ctrl2 = a.shift
-    return [a[6], a[5], a[4], a[3], 0x3f & a[2], a[1], a[0], ctrl2]
+
+    data = Array.new
+    i = 0
+    [a[6], a[5], a[4], a[3], 0x3f & a[2], a[1], a[0], ctrl2].each do |num|
+      data[i] = sprintf('%02x', num)
+      i += 1
+    end    
+    return data
   end
 
-  def write(date)
+  def write( idate )
+
+    date = Array.new
+    # BCD データへの変換
+    i = 0
+    idate.each do |num|
+      date[i] = (num / 10).to_i(2) << 4 | (num % 10).to_i(2)
+      i += 1
+    end
+    
     @i2c.write(0x32, [0x00, date[6], date[5], 0x80 | date[4], date[3], date[2], date[1], date[0]])
     sleep 0.1
     @i2c.write(0x32, [0xF0, 0x00])
--- samples/master.rb
+++ samples/master.rb
@@ -5,12 +5,12 @@ i2c = I2C.new(22, 21)
 
 # RTC 初期化. 時刻設定
 rtc = RX8035SA.new(i2c)
-rtc.write([0x20, 0x03, 0x31, 1, 0x23, 0x59, 0x50]) #年(下2桁), 月, 日, 曜日, 時, 分, 秒
+rtc.write([20, 3, 31, 1, 23, 59, 50]) #年(下2桁), 月, 日, 曜日, 時, 分, 秒
 
 while true
   tt = rtc.read
-  puts sprintf("%02x-%02x-%02x", tt[0], tt[1], tt[2])
-  puts sprintf("%02x:%02x:%02x", tt[4], tt[5], tt[6])
+  puts sprintf("%02d-%02d-%02d", tt[0], tt[1], tt[2])
+  puts sprintf("%02d:%02d:%02d", tt[4], tt[5], tt[6])
   puts ""
   sleep 1.0
 end
